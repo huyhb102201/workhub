3f1cd8cafbe155668d185d975f41bda420cf9e31
Ho Gia Huy <huyhg101004@gmail.com>
2025-09-15 14:29:25 +0700

    fixxxxxxx

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,7 +4,7 @@ return [
   'paths' => ['api/*'],
   'allowed_methods' => ['*'],
   'allowed_headers' => ['*'],
-  'allowed_origins' => ['http://localhost:5173','http://127.0.0.1:5173','app.vanda.id.vn','www.vanda.id.vn'],
+  'allowed_origins' => ['http://localhost:5173','http://127.0.0.1:5173','https://www.vanda.id.vn','https://app.vanda.id.vn',],
   'supports_credentials' => false,
 ];
 
